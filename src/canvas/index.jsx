8a71bf2b88dfe3ae63d8fc93e18d24327f1826f9
solomonadola <solomonadola84@gmil.com>
2023-08-07 11:21:21 +0300

    Setting 3d canvas completed

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,12 +1,23 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+/* eslint-disable react/no-unknown-property */
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+import Shirt from "./Shirt";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
 
-const Canvas = () => {
+const CanvasModel = () => {
   return (
-    <div>
-      Canvas
-    </div>
-  )
-}
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+      {/* <CameraRig> */}
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      {/* </CameraRig> */}
+    </Canvas>
+  );
+};
 
-export default Canvas
+export default Canvas;
